4a0c782a611e4a1bb109389cdb5a1f0c4d89d8a5
Keith <kayzee@live.com>
2019-02-09 23:55:05 -0800

    Problem3 solved

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   var addition = a + b;
-  console.log(addition);
   var message = 'The sum of '  + a +  " and " + b + ' is ' + addition + '.';
-  var message1 = 'The sum of 4 and 7 is 11.';
   return ([addition, message]);
 }
 
@@ -28,7 +26,6 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
   var multip = a * b;
-  console.log(multip);
   var messageMul = 'The product of ' + a +  " and " + b + ' is ' + multip + '.';
   return ([multip, messageMul]);
 }
@@ -48,11 +45,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sumOfabc = sum(sum(a, b)[0], c)[0];
+  var multiOfabc = multiply(multiply(a, b)[0], c)[0];
+  var message1 = a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfabc + '.';
+  var message2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiOfabc + '.';
+  return ([sumOfabc, multiOfabc, message1, message2]);
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
